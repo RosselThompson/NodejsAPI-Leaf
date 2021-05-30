a9f1a83d3fb20b379a734f5e5a4229b166364720
Rossel Thompson <rossel96t@gmail.com>
2021-05-30 00:53:51 -0600

    add prettier script

--- src/controllers/v1/userController.js
+++ src/controllers/v1/userController.js
@@ -1,9 +1,23 @@
-const createUser = (req, res) => { res.status(201).send({ message: 'User has been saved' }); };
-const getUsers = (req, res) => { res.status(200).send({ data: [] }); };
-const getUserById = (req, res) => { res.status(200).send({ data: {} }); };
-const updateUser = (req, res) => { res.status(200).send({ data: {} }); };
-const deleteUser = (req, res) => { res.status(200).send({ message: 'User has been deleted' }); };
+const createUser = (req, res) => {
+  res.status(201).send({ message: 'User has been saved' });
+};
+const getUsers = (req, res) => {
+  res.status(200).send({ data: [] });
+};
+const getUserById = (req, res) => {
+  res.status(200).send({ data: {} });
+};
+const updateUser = (req, res) => {
+  res.status(200).send({ data: {} });
+};
+const deleteUser = (req, res) => {
+  res.status(200).send({ message: 'User has been deleted' });
+};
 
 module.exports = {
-  createUser, getUsers, getUserById, updateUser, deleteUser
+  createUser,
+  getUsers,
+  getUserById,
+  updateUser,
+  deleteUser,
 };
--- src/routes/v1/index.js
+++ src/routes/v1/index.js
@@ -2,7 +2,6 @@ const userRoute = require('./userRoute');
 const companyRoute = require('./companyRoute');
 
 const prefix = '/api/v1';
-
 module.exports = (app) => {
   app.use(`${prefix}/user`, userRoute);
   app.use(`${prefix}/company`, companyRoute);
